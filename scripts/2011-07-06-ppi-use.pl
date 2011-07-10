f6621c18e372ccc33cd77f02ad8148edc91d7737
Roman Hubacek <roman.hubacek@centrum.cz>
2011-07-10 21:27:44 +0100

    Reformatted PPI usage

--- scripts/2011-07-06-ppi-use.pl
+++ scripts/2011-07-06-ppi-use.pl
@@ -4,9 +4,9 @@ use PPI;
 
 my %sub;
 my $Document = PPI::Document->new($ARGV[0]) or die "oops";
-for my $sub ( @{ $Document->find('PPI::Statement::Sub') || [] } ) {
-    unless ( $sub->forward ) {
-        $sub{ $sub->name } = $sub->content;
+for my $sub (@{$Document->find('PPI::Statement::Sub') || []}) {
+    unless ($sub->forward) {
+        $sub{$sub->name} = $sub->content;
     }
 }
 

8003de6e20ada4357174d873fd7b19e5d9c9c7be
Roman Hubacek <roman.hubacek@centrum.cz>
2011-08-22 16:29:11 +0200

    Updated prime checking algorithm.

--- scripts/2011-08-21-pe7.pl
+++ scripts/2011-08-21-pe7.pl
@@ -16,7 +16,7 @@ my @primes = (2);
 my $cur = 3;
 while(@primes <= 10_001) {
     push @primes, $cur
-        if is_prime($cur, \@primes);
+        if is_prime($cur);
     $cur+=2;
 }
 
@@ -24,10 +24,31 @@ dd [@primes[0..20]];
 say $primes[6 - 1];
 say $primes[10_001 - 1];
 
-sub is_prime {
+sub is_prime_naive {
     my ($n,$primes) = @_;
     for(@$primes) {
         return 0 if $n % $_ == 0;
     }
     return 1;
+}
+
+# from projecteuler.net 007_overview.pdf
+sub is_prime {
+    my $n = shift;
+
+    return 0 if $n == 1;
+    return 1 if $n < 4;        # 2 and 3 are prime
+    return 0 if $n % 2 == 0;
+    return 1 if $n < 9;        # we have already excluded 4,6 and 8.
+    return 0 if $n % 3 == 0;
+
+    # n rounded to the greatest integer r so that r*r<=n
+    my $r = int(sqrt($n));
+    my $f = 5;
+    while ($f <= $r) {
+        return 0 if $n % $f == 0;
+        return 0 if $n % ($f + 2) == 0;
+        $f += 6;
+    }
+    return 1              # in all other cases
 }

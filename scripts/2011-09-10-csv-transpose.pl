383a0098b893e3b7fe34d1d2326de7455da87526
Roman Hubacek <roman.hubacek@centrum.cz>
2011-09-14 06:36:04 +0200

    CSV transpose

---
+++ scripts/2011-09-10-csv-transpose.pl
@@ -0,0 +1,24 @@
+
+use Text::CSV_XS;
+
+my $csv = Text::CSV_XS->new({ binary => 1, sep_char => ';' })
+    or die;
+
+my @header = @{ $csv->getline(\*DATA) };
+my @data   = ( [ @header ] );
+while (my $row = $csv->getline(\*DATA)) {
+    push @data, $row;
+}
+
+for my $col (0 .. $#header) {
+    $csv->combine( map { $data[$_][$col] } 0 .. $#data );
+    print $csv->string,"\n";
+}
+
+__DATA__
+id;Column1;column2;column3;columnN
+1;apple;Red;Medium;Text1
+2;Mango;Yellow;Large;Text2
+3;Banana;Yellow;small;Text3
+4;Apple;Red;Medium;Text4
+5;Pear;Green;Medium;Text5

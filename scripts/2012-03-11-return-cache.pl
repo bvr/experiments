f74d73f83fa4eb3514195a75d3328574cc6c90e8
Roman Hubacek <roman.hubacek@centrum.cz>
2012-03-11 20:41:41 +0100

    Caching of DBIC stored value.

---
+++ scripts/2012-03-11-return-cache.pl
@@ -0,0 +1,28 @@
+
+package Item;
+use Moose;
+
+sub cached {
+    my ($self, $cache_key, $cb) = @_;
+
+    if(defined(my $data = $self->{$cache_key})) {
+        return    $data;
+    }
+
+    # calculate item, store into cache and return it
+    return $self->{$cache_key} = $cb->($self);
+}
+
+sub payload_spare {
+    shift->cached('_payload_spare' => sub {
+        warn "calculation";
+        145
+    });
+}
+
+package main;
+
+my $item = Item->new();
+
+warn $item->payload_spare;
+warn $item->payload_spare;

ab61712de14dbe2f34ef1e43037099b493dd7e48
Roman Hubacek <roman.hubacek@centrum.cz>
2012-04-12 12:23:56 +0200

    Minor facelift of migration script.

--- scripts/2012-03-29-migrate_dddb.pl
+++ scripts/2012-03-29-migrate_dddb.pl
@@ -1,15 +1,13 @@
 use Config::Auto;
 use DBI;
 
-my %config = %{ Config::Auto::parse('do_sql.ini') };
-my $cfg = $config{fctools6w};
+my $config = Config::Auto::parse('do_sql.ini');
+my $cfg = $config->{fctools6w};
 
-my $db = 'datadict';
-my $dbh = DBI->connect("DBI:mysql:$db:" . $cfg->{serv}, $cfg->{user}, $cfg->{pass})
-    or die "Connect error: $DBI::errstr";
+my $dbh = DBI->connect("DBI:mysql:host=$cfg->{serv}", $cfg->{user}, $cfg->{pass});
 
-my $databases = $dbh->selectall_arrayref("SHOW DATABASES");
-for my $db_name (sort map { $_->[0] } @$databases) {
+my $databases = $dbh->selectcol_arrayref("SHOW DATABASES");
+for my $db_name (sort @$databases) {
     my $ret1 = $dbh->do("ALTER TABLE $db_name.producedobjects DROP KEY `Unq_Object`");
     my $ret2 = $dbh->do("ALTER TABLE $db_name.producedobjects ADD UNIQUE KEY `Unq_Produce` (`FunctionID`,`ObjectID`)");
     warn join("\t", $db_name, $ret1, $ret2), "\n";

cf2b403b3ec435aa91b5d2ca377738d836757206
Roman Hubacek <roman.hubacek@centrum.cz>
2012-05-27 19:52:13 +0200

    Bottles on the wall golfing.

---
+++ scripts/2012-05-08-99bottle.pl
@@ -0,0 +1,5 @@
+# from http://www.perlmonks.org/?node_id=903641
+print"@{sub b{[@y=($n||=99,bottle.'s'x1!~$n,of,beer),on,the,wall]}b}, @y.
+$_, @{b--$n}.
+
+"for("Take one down and pass it around")x98,"Go to the store and buy some more"

ed632205b18a73ee94d092c190c2c641f8ef915d
Roman Hubacek <roman.hubacek@honeywell.com>
2019-12-09 09:00:27 +0100

    Minor formatting update

--- scripts/2017-12-18-table-checksum.pl
+++ scripts/2017-12-18-table-checksum.pl
@@ -39,9 +39,16 @@ sub evenly_divisible {
 
 --- Day 2: Corruption Checksum ---
 
-As you walk through the door, a glowing humanoid shape yells in your direction. "You there! Your state appears to be idle. Come help us repair the corruption in this spreadsheet - if we take another millisecond, we'll have to display an hourglass cursor!"
+As you walk through the door, a glowing humanoid shape yells in your
+direction. "You there! Your state appears to be idle. Come help us repair the
+corruption in this spreadsheet - if we take another millisecond, we'll have to
+display an hourglass cursor!"
 
-The spreadsheet consists of rows of apparently-random numbers. To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum. For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
+The spreadsheet consists of rows of apparently-random numbers. To make sure the
+recovery process is on the right track, they need you to calculate the
+spreadsheet's checksum. For each row, determine the difference between the
+largest value and the smallest value; the checksum is the sum of all of these
+differences.
 
 For example, given the following spreadsheet:
 
@@ -61,11 +68,19 @@ Your puzzle answer was 45972.
 
 --- Part Two ---
 
-"Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
+"Great work; looks like we're on the right track after all. Here's a star for
+your effort." However, the program seems a little worried. Can programs be
+worried?
 
-"Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+"Based on what we're seeing, it looks like all the User wanted is some
+information about the evenly divisible values in the spreadsheet.
+Unfortunately, none of us are equipped for that kind of calculation - most of
+us specialize in bitwise operations."
 
-It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+It sounds like the goal is to find the only two numbers in each row where one
+evenly divides the other - that is, where the result of the division operation
+is a whole number. They would like you to find those numbers on each line,
+divide them, and add up each line's result.
 
 For example, given the following spreadsheet:
 
